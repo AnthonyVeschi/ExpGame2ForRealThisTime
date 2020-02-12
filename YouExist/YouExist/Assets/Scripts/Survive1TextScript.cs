50ca001543b9958afbbd0cd2e1a3cf03e3e1cce9
Anthony Veschi <anthonyveschi@gmail.com>
2020-02-12 04:43:34 -0500

    circlesUnite1 scene completed

---
+++ YouExist/YouExist/Assets/Scripts/Survive1TextScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Survive1TextScript : MonoBehaviour
+{
+    public GameObject staticVideo;
+
+    void Start()
+    {
+        StartCoroutine("ChangeScene");
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(2);
+        staticVideo.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("CirclesUnite1");
+    }
+}
---
+++ YouExist/YouExist/Assets/Scripts/WaitThenCry.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitThenCry : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("Cry");
+    }
+
+    IEnumerator Cry()
+    {
+        yield return new WaitForSeconds(0.15f);
+        gameObject.GetComponent<AudioSource>().enabled = true;
+    }
+}
