3f61b7263b49c894591032dfdb3279c2faa87b51
Anthony Veschi <anthonyveschi@gmail.com>
2020-02-12 07:31:11 -0500

    crowd scene finished

---
+++ YouExist/YouExist/Assets/Scripts/CrowdGuyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdGuyScript : MonoBehaviour
+{
+    public int speed = 12;
+
+    public int crySecs;
+
+    private void Start()
+    {
+        //StartCoroutine("TooShyShyHushHushCryToCry");
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transform.position = Vector2.MoveTowards(transform.position, other.transform.position, -1 * speed * Time.deltaTime);
+        }
+    }
+
+    IEnumerator TooShyShyHushHushCryToCry()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(crySecs);
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ YouExist/YouExist/Assets/Scripts/CryGuyScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CryGuyScript : MonoBehaviour
+{
+    bool CanPush = false;
+
+    public GameObject canvas;
+
+    void Start()
+    {
+        StartCoroutine("Cry");
+    }
+
+    IEnumerator Cry()
+    {
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponent<AudioSource>().Play();
+
+        yield return new WaitForSeconds(1);
+        CanPush = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && CanPush)
+        {
+
+            gameObject.GetComponent<Animator>().SetTrigger("Push");
+            StartCoroutine("ChangeScene");
+        }
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(1);
+        canvas.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("SexScene");
+    }
+}
