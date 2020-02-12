0ff94b164f5cbcb623aa9d9580205371fb6f7bd0
Anthony Veschi <anthonyveschi@gmail.com>
2020-02-12 02:20:34 -0500

    first scene + transition scene fully done

---
+++ YouExist/YouExist/Assets/Scripts/CanSupportLifeScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanSupportLifeScript : MonoBehaviour
+{
+    Text t;
+
+    private Color32 red;
+    private Color32 green;
+    private Color32 white;
+
+    string emptyString;
+    string waitString;
+    string noLifeString;
+    string yesLifeString;
+
+    private void Start()
+    {
+        t = gameObject.GetComponent<Text>();
+
+        white = new Color32(255, 255, 255, 255);
+        red = new Color32(255, 200, 200, 255);
+        green = new Color32(200, 255, 200, 255);
+
+        emptyString = "";
+        waitString = "please wait . . .";
+        noLifeString = "this universe cannot support life";
+        yesLifeString = "this universe can support life";
+
+        t.text = emptyString;
+    }
+
+    public void PleaseWait()
+    {
+        t.text = waitString;
+        t.color = white;
+    }
+    public void NoLife()
+    {
+        t.text = noLifeString;
+        t.color = red;
+    }
+    public void YesLife()
+    {
+        t.text = yesLifeString;
+        t.color = green;
+    }
+}
--- YouExist/YouExist/Assets/Scripts/DetermineButtonScript.cs
+++ YouExist/YouExist/Assets/Scripts/DetermineButtonScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DetermineButtonScript : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class DetermineButtonScript : MonoBehaviour
     public GameObject n;
     private Button b;
     private FundamentalsNumbersScript fns;
+
+    public GameObject canSupportLife;
 
     public GameObject electron;
     private Animator anim;
@@ -59,18 +62,30 @@ public class DetermineButtonScript : MonoBehaviour
                 break;
             case (6):
                 anim.SetTrigger("Orbit");
+                StartCoroutine("ChangeScene");
                 break;
         }
-        StartCoroutine("Wait");
-
         i++;
+        StartCoroutine("Wait");
     }
 
     IEnumerator Wait()
     {
         canPress = false;
+        canSupportLife.GetComponent<CanSupportLifeScript>().PleaseWait();
+
         yield return new WaitForSeconds(1f);
         anim.SetTrigger("ReturnToIdle");
-        canPress = true;
+
+        if (i == 7) { canSupportLife.GetComponent<CanSupportLifeScript>().YesLife(); }
+        else { canSupportLife.GetComponent<CanSupportLifeScript>().NoLife(); }
+
+        if (i < 7) { canPress = true; }
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("StaticFromUniverse");
     }
 }
