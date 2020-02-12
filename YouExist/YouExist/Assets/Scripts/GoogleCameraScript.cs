ebcd5a678b98636c49e27becc681f402af331d27
Anthony Veschi <anthonyveschi@gmail.com>
2020-02-12 08:45:41 -0500

    it's done if i want it to be

---
+++ YouExist/YouExist/Assets/Scripts/GoogleCameraScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoogleCameraScript : MonoBehaviour
+{
+    public GameObject canvas;
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+        StartCoroutine("End");
+    }
+
+    IEnumerator End()
+    {
+        yield return new WaitForSeconds(3);
+        canvas.SetActive(true);
+        cam.fieldOfView = 160;
+        yield return new WaitForSeconds(1);
+        canvas.SetActive(false);
+
+        yield return new WaitForSeconds(3);
+        canvas.SetActive(true);
+        cam.fieldOfView = 300;
+        yield return new WaitForSeconds(1);
+        canvas.SetActive(false);
+
+        yield return new WaitForSeconds(3);
+        canvas.SetActive(true);
+        cam.fieldOfView = 9999;
+        yield return new WaitForSeconds(1);
+
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Menu");
+    }
+}
