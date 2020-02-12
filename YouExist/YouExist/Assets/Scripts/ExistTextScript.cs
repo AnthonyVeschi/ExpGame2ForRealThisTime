44990f5b1d4064d6039150b37e74c2a7fe4966f1
Anthony Veschi <anthonyveschi@gmail.com>
2020-02-12 03:58:28 -0500

    exist scene completed

---
+++ YouExist/YouExist/Assets/Scripts/ExistTextScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExistTextScript : MonoBehaviour
+{
+    public GameObject staticVideo;
+
+    void Start()
+    {
+        StartCoroutine("Survive");
+    }
+
+    IEnumerator Survive()
+    {
+        yield return new WaitForSeconds(4);
+        gameObject.GetComponent<Text>().text = "SURVIVE";
+
+        yield return new WaitForSeconds(1);
+        staticVideo.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("SurviveScene1");
+    }
+}
